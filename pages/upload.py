d404c12df1ad87af9e587ea80a7432cbad3c07b2
Filbert Chan <120439858+filbertchan@users.noreply.github.com>
2025-10-27 16:59:16 +0800

    Update upload.py

--- pages/upload.py
+++ pages/upload.py
@@ -10,6 +10,9 @@ st.subheader("Direct upload to S3")
 # AWS credentials and bucket
 AWS_ACCESS_KEY = st.secrets["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_KEY = st.secrets["AWS_SECRET_ACCESS_KEY"]
+USER_POOL_ID = st.secrets["USER_POOL_ID"]
+APP_CLIENT_ID = st.secrets["APP_CLIENT_ID"] 
+APP_CLIENT_SECRET = st.secrets["APP_CLIENT_SECRET"]
 AWS_REGION = "us-east-1"   # change if needed
 BUCKET_NAME = "aws-architect-agent-requirement"
 
@@ -24,6 +27,21 @@ uploaded_file = st.file_uploader("Upload PDF, DOCX, or TXT file", type=["pdf", "
 
 if uploaded_file is not None:
     st.info(f"📁 Selected: {uploaded_file.name}")
+    if st.session_state["logged_in"] == False: 
+        try:
+        response = client.initiate_auth(
+            AuthFlow="USER_PASSWORD_AUTH",
+            AuthParameters={"USERNAME": username, "PASSWORD": password},
+            ClientId=CLIENT_ID
+        )
+        token = response["AuthenticationResult"]["AccessToken"]
+        st.session_state["logged_in"] = True
+        st.session_state["access_token"] = token
+        st.success("✅ Login successful! Go to the Upload page.")
+    except client.exceptions.NotAuthorizedException:
+        st.error("❌ Invalid username or password.")
+    except Exception as e:
+        st.error(f"⚠️ Error: {e}")
     if st.button("Upload to S3"):
         try:
             s3.upload_fileobj(uploaded_file, BUCKET_NAME, f"documents/{uploaded_file.name}")
